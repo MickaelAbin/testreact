afab56be37ac948773fb31eea9b380e4b50a2f83
Mickaël Abin <mab@ucaya.com>
2024-11-22 13:22:48 +0100

    Ajout useState

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,24 +1,29 @@
 import '../styles/Cart.css'
+import { useState } from 'react'
 
 
-function Cart(){
-    const monsteraprice = 8
-    const lierreprice = 10
-    const bouquetprice = 15
-return (
-    <div className='lmj-cart'> 
-        <h2>Panier</h2>
-        <ul>
-            <li>Monstera : {monsteraprice} euros</li>
-            <li>Lierre : {lierreprice} euros</li>
-            <li>Bouquet de fleurs : {bouquetprice} euros</li>
-        </ul>
-      
-      Prix total : {monsteraprice+lierreprice+bouquetprice} euros
-        
-        
-    </div>);
-    
-
+function Cart() {
+    const monsteraPrice = 8
+    const [cart, updateCart] = useState(0)
+    const [isOpen, setIsOpen] = useState(false)
+ 
+    return isOpen ? (
+        <div className='lmj-cart'>
+            <button onClick={() => setIsOpen(false)}>Fermer</button>
+            <h2>Panier</h2>
+            <div>
+                Monstera : {monsteraPrice}€
+                <button onClick={() => updateCart(cart + 1)}>
+                    Ajouter
+                </button>
+            </div>
+            <h3>Total : {monsteraPrice * cart}€</h3>
+            <button onClick={() => updateCart(0)}>
+                    Réinitialisé
+                </button>
+        </div>
+    ) : (
+        <button onClick={() => setIsOpen(true)}>Ouvrir le Panier</button>
+    )
 }
 export default Cart
