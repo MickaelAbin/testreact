960055cc24e7291a98dae69e506102167f43b705
makel44 <120366866+makel44@users.noreply.github.com>
2024-12-16 19:32:05 +0100

    test pour quick app distribution

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,131 @@
-import Banner from './Banner'
-import logo from '../assets/logo.png'
-// import Cart from './Cart'
-import ShoppingList from './ShoppingList'
+import React, { useEffect, useState } from 'react';
+import './App.css'; // Assure-toi d'importer le fichier CSS
 
 function App() {
-	return (
-		<div>
-			<Banner>
-				<img src={logo} alt='La maison jungle' className='lmj-logo' />
-				<h1 className='lmj-title'>La maison jungle</h1>
-			</Banner>
-			{/* <Cart /> */}
-			<ShoppingList />
-		</div>
-	)
+    const [apps, setApps] = useState([]);
+    const [versions, setVersions] = useState([]);
+    const [selectedApp, setSelectedApp] = useState('');
+    const [selectedPlatform, setSelectedPlatform] = useState('');
+    const [platforms, setPlatforms] = useState({});
+
+    useEffect(() => {
+        fetch('http://localhost:3000/releases/list')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Données récupérées :', data);
+                setApps(data);
+
+                // Vérifier les plateformes pour chaque application
+                data.forEach(app => {
+                    fetch(`http://localhost:3000/check-platforms/${app}`)
+                        .then(response => response.json())
+                        .then(platformData => {
+                            setPlatforms(prevPlatforms => ({ ...prevPlatforms, [app]: platformData }));
+                        })
+                        .catch(error => console.error('Erreur lors de la vérification des plateformes:', error));
+                });
+            })
+            .catch(error => console.error('Erreur lors de la récupération des applications:', error));
+    }, []);
+
+    const fetchVersions = (appName, platform) => {
+        fetch(`http://localhost:3000/release/${appName}/${platform}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(`Versions pour ${appName} sur ${platform} :`, data);
+
+                // Trier les versions par date de modification décroissante (la plus récente en premier)
+                const sortedVersions = data.sort((a, b) => new Date(b.mtime) - new Date(a.mtime));
+
+                setSelectedApp(appName);
+                setSelectedPlatform(platform);
+                setVersions(sortedVersions);
+            })
+            .catch(error => console.error('Erreur lors de la récupération des versions:', error));
+    };
+
+    return (
+        <div className="container">
+            <div className="apps-list">
+                <h1>Applications Disponibles</h1>
+                <ul>
+                    {apps.length > 0 ? (
+                        apps.map(app => (
+                            <li key={app} className="app-item">
+                                <span>{app}</span>
+                                <span className="logos">
+                                    {platforms[app]?.Android && (
+                                        <img
+                                            src="android-logo.png"
+                                            alt="Android"
+                                            className="logo"
+                                            onClick={() => fetchVersions(app, 'Android')}
+                                        />
+                                    )}
+                                    {platforms[app]?.IOS && (
+                                        <img
+                                            src="ios-logo.png"
+                                            alt="iOS"
+                                            className="logo"
+                                            onClick={() => fetchVersions(app, 'iOS')}
+                                        />
+                                    )}
+                                </span>
+                            </li>
+                        ))
+                    ) : (
+                        <li>Aucune application disponible</li>
+                    )}
+                </ul>
+            </div>
+
+            <div className="versions-list">
+                {versions.length > 0 && (
+                    <div>
+                        <h2>Versions disponibles pour {selectedApp} sur {selectedPlatform}</h2>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Version</th>
+                                    <th>Release</th>
+                                    <th>Date</th>
+                                    <th>Télécharger</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {versions.map(({ version, birthtime }) => {
+                                    const versionParts = version.split('.');
+                                    const releaseNumber = versionParts.pop(); // Dernier groupe de chiffres
+                                    const versionNumber = versionParts.join('.'); // Les trois premiers groupes de chiffres
+                                    const downloadUrl = selectedPlatform === 'Android'
+                                        ? `http://localhost:3000/releases/${selectedApp}/Android/${version}/${selectedApp}.apk`
+                                        : `http://localhost:3000/releases/${selectedApp}/IOS/${version}/manifest.plist`;
+                                    return (
+                                        <tr key={version}>
+                                            <td>{versionNumber}</td>
+                                            <td>{releaseNumber}</td>
+                                            <td>{new Date(birthtime).toLocaleString()}</td>
+                                            <td>
+                                                <a href={downloadUrl} download>
+                                                    Télécharger
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    );
+
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -3,7 +3,7 @@ import '../styles/PlantItem.css'
 
 function PlantItem({ id, cover, name, water, light }) {
 	return (
-		<li key={id} className='lmj-plant-item' onClick={() => handleClick(name)}>
+		<li key={id} className='lmj-plant-item'onClick={() => handleClick(name)} >
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
@@ -17,4 +17,5 @@ function PlantItem({ id, cover, name, water, light }) {
 function handleClick(plantName) {
     alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix 🌱✨`)
 }
+
 export default PlantItem
